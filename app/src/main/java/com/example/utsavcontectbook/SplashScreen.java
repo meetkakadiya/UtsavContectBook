bff7845e6c640154f5bb48f34dbb6771d1413379
Akshay Kalola <akshay.kalola28@gmail.com>
2019-10-20 18:10:36 +0530

    splash image seen as a full screen on notification also

--- app/src/main/java/com/example/utsavcontectbook/SplashScreen.java
+++ app/src/main/java/com/example/utsavcontectbook/SplashScreen.java
@@ -3,13 +3,14 @@ package com.example.utsavcontectbook;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
@@ -20,6 +21,11 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
