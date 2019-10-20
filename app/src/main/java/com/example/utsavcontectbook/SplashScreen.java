7b2710e2459ce0583308e960f1050a94cd3fb928
Akshay Kalola <akshay.kalola28@gmail.com>
2019-10-20 17:51:54 +0530

    call permission set

--- app/src/main/java/com/example/utsavcontectbook/SplashScreen.java
+++ app/src/main/java/com/example/utsavcontectbook/SplashScreen.java
@@ -5,10 +5,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -22,20 +23,32 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-
                 if (ContextCompat.checkSelfPermission(SplashScreen.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(SplashScreen.this, new String[]{Manifest.permission.CALL_PHONE}, 100);
                 }
-                Intent mainIntent = new Intent(SplashScreen.this, HomeActivity.class);
-                SplashScreen.this.startActivity(mainIntent);
-                SplashScreen.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
 
+    private void openHome() {
+        /* Create an Intent that will start the Menu-Activity.*/
+        Intent mainIntent = new Intent(SplashScreen.this, HomeActivity.class);
+        SplashScreen.this.startActivity(mainIntent);
+        SplashScreen.this.finish();
+    }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 100: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openHome();
+                } else {
+                    Toast.makeText(SplashScreen.this, "You have must allow to Call from the Application.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
     }
 }
